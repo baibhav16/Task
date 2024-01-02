669076a1c9fe0b7f630894df1184afde4439cad1
Baibhav Kumar <147192139+baibhav16@users.noreply.github.com>
2024-01-02 15:51:37 +0530

    Update Gradient.js

--- components/Gradient.js
+++ components/Gradient.js
@@ -4,7 +4,7 @@ function Gradient() {
   return (
     <div className="absolute w-screen h-[3072px] ">
           <img className="absolute w-4/6 h-1/2 top-15 left-0" alt="Vector" src="/vector-8.png" />
-          <img className="absolute w-3/5 h-7/12 top-0 left-[41%]" alt="Vector" src="/vector-9.png" />
+          <img className="absolute w-3/5 h-7/12 top-0 left-[40%]" alt="Vector" src="/vector-9.png" />
           <img
             className="absolute w-svw h-1/2 top-1/2 "
             alt="Ellipse"
