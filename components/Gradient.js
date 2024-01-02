b315c268df0a3dc3c91a3a235e3602f12921111f
Baibhav Kumar <147192139+baibhav16@users.noreply.github.com>
2024-01-02 15:51:51 +0530

    Update Gradient.js

--- components/Gradient.js
+++ components/Gradient.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Gradient() {
   return (
-    <div className="absolute w-screen h-[3072px] ">
+    <div className="absolute w-full h-[3072px] ">
           <img className="absolute w-4/6 h-1/2 top-15 left-0" alt="Vector" src="/vector-8.png" />
           <img className="absolute w-3/5 h-7/12 top-0 left-[40%]" alt="Vector" src="/vector-9.png" />
           <img
